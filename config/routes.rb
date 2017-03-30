4dca8531905be659725e138b96176a97fac4a546
w3tra <w3tra@mail.ru>
2017-03-30 19:31:53 +0600

    adding locale change links

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  root 'products#index'
+	scope ':locale', locale: /#{I18n.available_locales.join("|")}/ do
+	  root 'products#index'
 
-  devise_for :users
+	  devise_for :users
 
-  namespace :admin do
-    root 'products#index'
-    resources :products
-    resources :categories
-  end
+	  namespace :admin do
+	    root 'products#index'
+	    resources :products
+	    resources :categories
+	  end
+	end
+	match "*path", to: redirect("/#{I18n.default_locale}/%{path}"), via: :all
+ 	match "", to: redirect("/#{I18n.default_locale}/"), via: :all
 end
