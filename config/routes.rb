102dd1d182444a74ca384394f914abfee50e741c
w3tra <w3tra@mail.ru>
2017-03-30 19:45:02 +0600

    editing devise locales

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
 	scope ':locale', locale: /#{I18n.available_locales.join("|")}/ do
 	  root 'products#index'
 
-	  devise_for :users
-
 	  namespace :admin do
 	    root 'products#index'
 	    resources :products
 	    resources :categories
 	  end
 	end
+	devise_for :users
 	match "*path", to: redirect("/#{I18n.default_locale}/%{path}"), via: :all
  	match "", to: redirect("/#{I18n.default_locale}/"), via: :all
 end
